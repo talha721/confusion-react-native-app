d0682e2fe8a7ae3d47d5fba7e5a7d764e0351466
talha721 <talhamoazzam4118@gmail.com>
2022-06-04 20:16:05 +0500

    Navigation Part 1

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,55 +1,39 @@
-import React, { Component, useState } from "react";
+import React, { Component } from "react";
 import Menu from "./MenuComponent";
-import { DISHES } from "../shared/dishes";
 import DishDetail from "./DishDetailComponent";
-import { View } from "react-native";
+import { View, Platform } from "react-native";
+import { createStackNavigator } from "react-navigation";
 
-// class Main extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       dishes: DISHES,
-//       selectedDish: null,
-//     };
-//   }
-//
-//   onDishSelect(dishId) {
-//     this.setState({ selectedDish: dishId });
-//   }
-//
-//   render() {
-//     return (
-//       <View>
-//         <Menu
-//           dishes={this.state.dishes}
-//           onPress={(dishId) => this.onDishSelect(dishId)}
-//         />
-//         <DishDetail
-//           dish={
-//             this.state.dishes.filter(
-//               (dish) => dish.id === this.state.selectedDish
-//             )[0]
-//           }
-//         />
-//       </View>
-//     );
-//   }
-// }
+const MenuNavigator = createStackNavigator(
+  {
+    Menu: { screen: Menu },
+    Dishdetail: { screen: DishDetail },
+  },
+  {
+    initialRouteName: "Menu",
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: "#512DA8",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: { color: "#fff" },
+    },
+  }
+);
 
-const Main = () => {
-  const [dishes] = useState(DISHES);
-  const [selectedDish, setSelectedDish] = useState(null);
-
-  const onDishSelect = (dishId) => {
-    setSelectedDish(dishId);
-  };
-
-  return (
-    <View>
-      <Menu dishes={dishes} onPress={(dishId) => onDishSelect(dishId)} />
-      <DishDetail dish={dishes.filter((dish) => dish.id === selectedDish)[0]} />
-    </View>
-  );
-};
+class Main extends Component {
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          paddingTop: Platform.OS === "android" ? 35 : 0,
+        }}
+      >
+        <MenuNavigator />
+      </View>
+    );
+  }
+}
 
 export default Main;
