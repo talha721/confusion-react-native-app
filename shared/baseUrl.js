a3b72b4584389b2f8da60710de749c319359a896
talha721 <talhamoazzam4118@gmail.com>
2022-06-08 13:17:39 +0500

    fix baseURL issue

--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,4 +1,5 @@
 // export const baseUrl = "http://localhost:3001/";
-export const baseUrl = "http://10.0.2.2:3001/"; // working
-// export const baseUrl = "http://192.168.10.19:3001/"; // LAN
+export const baseUrl = "http://10.0.2.2:8000/"; // working
+// export const baseUrl = "http://192.168.10.19:8000/"; // LAN
 // export const baseUrl = "http://192.168.10.17:3001/"; // WIFI
+// export const baseUrl = "http://192.168.1.114:3001/";
