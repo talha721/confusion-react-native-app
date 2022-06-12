1c5d915e9bef82956e9bf22dc6fb6df15069c1ca
talha721 <talhamoazzam4118@gmail.com>
2022-06-12 22:44:59 +0500

    Delete Favorites

--- components/FavoriteComponent.js
+++ components/FavoriteComponent.js
@@ -4,6 +4,8 @@ import { ListItem } from "react-native-elements";
 import { connect } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
 import { Loading } from "./LoadingComponent";
+import Swipeout from "react-native-swipeout";
+import { deleteFavorite } from "../redux/Actions/favoriteActions";
 
 const mapStateToProps = (state) => {
   return {
@@ -11,6 +13,10 @@ const mapStateToProps = (state) => {
     favorites: state.favorites,
   };
 };
+
+const mapDispatchToProps = (dispatch) => ({
+  deleteFavorite: (dishId) => dispatch(deleteFavorite(dishId)),
+});
 
 class Favorites extends Component {
   constructor(props) {
@@ -25,15 +31,25 @@ class Favorites extends Component {
     const { navigate } = this.props.navigation;
 
     const renderMenuItem = ({ item, index }) => {
+      const rightButton = [
+        {
+          text: "Delete",
+          type: "delete",
+          onPress: () => this.props.deleteFavorite(item.id),
+        },
+      ];
+
       return (
-        <ListItem
-          key={index}
-          title={item.name}
-          subtitle={item.description}
-          hideChevron={true}
-          onPress={() => navigate("Dishdetail", { dishId: item.id })}
-          leftAvatar={{ source: { uri: baseUrl + item.image } }}
-        />
+        <Swipeout right={rightButton} autoClose={true}>
+          <ListItem
+            key={index}
+            title={item.name}
+            subtitle={item.description}
+            hideChevron={true}
+            onPress={() => navigate("Dishdetail", { dishId: item.id })}
+            leftAvatar={{ source: { uri: baseUrl + item.image } }}
+          />
+        </Swipeout>
       );
     };
 
@@ -59,4 +75,4 @@ class Favorites extends Component {
   }
 }
 
-export default connect(mapStateToProps)(Favorites);
+export default connect(mapStateToProps, mapDispatchToProps)(Favorites);
